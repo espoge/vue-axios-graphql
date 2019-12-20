0e4469f254da3ddbb33ae4d65dea6ba9af3ce183
Gennaro Esposito <gespositoe@ugmlocal.com>
2019-12-20 11:46:36 +0100

    my first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,115 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
+    <div>
+      <label for="">firstname
+        <input type="text" v-model="firstname">
+      </label>
+      <label for="">lastname
+        <input type="text" v-model="lastname">
+      </label>
+      <button @click="add">Add</button>
+    </div>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+      <li v-for="(user,i) in users" :key="i">
+        {{user.firstname}}  {{user.lastname}} <button  @click="detail(user.id)">Seleziona</button>
+        </li>
     </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    {{user}}
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data(){
+    return{
+      users:[],
+      user:{},
+      firstname:'',
+      lastname:''
+    }
+  },
+  async mounted(){
+    try{
+     let res = await axios({
+        method: "POST",
+        url:"http://127.0.0.1:3333/graphql",
+        data:{
+         query: `
+          query allUsers {
+            allUsers {
+              id
+              firstname,
+              lastname
+              }
+            }
+          `
+        }
+      })
+    this.users= res.data.data.allUsers
+
+    }catch(e) {
+      console.log(e)
+    }
+  },
+  methods:{
+    async detail(id){
+      try{
+     let res = await axios({
+        method: "POST",
+        url:"http://127.0.0.1:3333/graphql",
+        data:{
+         query: `
+          query fetchUser {
+            fetchUser(id:${id}){
+              id
+              firstname,
+              lastname,
+              job{
+                description
+                }
+              }
+            }
+          `
+        }
+      })
+     console.log(res.data.data.fetchUser)
+    this.user= res.data.data.fetchUser
+
+    }catch(e) {
+      console.log(e)
+    }
+    },
+
+ async add(){
+    try{
+      let res = await axios({
+         method: "POST",
+        url:"http://127.0.0.1:3333/graphql",
+        data:{
+         query: `
+          mutation {
+          createUser(firstname:"${this.firstname}",lastname:"${this.lastname}"){
+          firstname
+          lastname
+          }
+        } 
+         `
+        }
+      })
+     console.log(res)
+
+    }catch(e){
+      console.log(e)
+    }
+
   }
+
+
+  }
+ 
+
 }
 </script>
 
